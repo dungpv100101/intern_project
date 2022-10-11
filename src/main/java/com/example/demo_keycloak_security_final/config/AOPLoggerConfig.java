75f21b8f83f8f5fbffa5e9639f7bca051d2b8f38
dungpv100101 <dungpv100101@gmail.com>
2022-10-11 16:56:25 +0700

    20221011_addSaveLogToFile

--- src/main/java/com/example/demo_keycloak_security_final/config/AOPLoggerConfig.java
+++ src/main/java/com/example/demo_keycloak_security_final/config/AOPLoggerConfig.java
@@ -3,26 +3,26 @@ package com.example.demo_keycloak_security_final.config;
 import com.example.demo_keycloak_security_final.util.StringUtil;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
-
 @Aspect
 @Component
 public class AOPLoggerConfig {
+    private final Logger logger = LoggerFactory.getLogger(AOPLoggerConfig.class);
+
     @Before("execution(* com.example.demo_keycloak_security_final..*.*(..))")
     public void logBeforeMethod(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String args = Arrays.toString(joinPoint.getArgs());
 
-        System.out.println("Executing: " +
-                className +
-                StringUtil.SPACE_CHARACTER +
-                methodName +
-                " with args: " +
-                args);
+        String logString = "Executing: " + className + StringUtil.SPACE_CHARACTER + methodName + " with args: " + args;
+
+        logger.info(logString);
     }
 
     @AfterReturning(value = "execution(* com.example.demo_keycloak_security_final..*.*(..))", returning = "result")
@@ -30,11 +30,19 @@ public class AOPLoggerConfig {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getSignature().getDeclaringTypeName();
 
-        if (result != null) System.out.println("Return: " +
-                className +
-                StringUtil.SPACE_CHARACTER +
-                methodName +
-                StringUtil.SPACE_CHARACTER +
-                result);
+        if (result != null) {
+            String logString = "Return: " + className + StringUtil.SPACE_CHARACTER + methodName + StringUtil.SPACE_CHARACTER + result;
+            logger.info(logString);
+        }
+    }
+
+    @AfterThrowing(value = "execution(* com.example.demo_keycloak_security_final..*.*(..))", throwing = "exception")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+
+        String logString = "Error: " + className + StringUtil.SPACE_CHARACTER + methodName + StringUtil.SPACE_CHARACTER + exception.toString();
+
+        logger.error(logString);
     }
 }
